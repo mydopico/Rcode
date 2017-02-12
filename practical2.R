446d8ed472fd2982570dabc2984d214dc5f96bb4
mydopico <mydopico@gmail.com>
2017-02-12 10:35:05 +0000

    Update practical2.R
    Add exercises

--- practical2.R
+++ practical2.R
@@ -12,8 +12,8 @@ data.df[data.df$"age">43,]
 # Command tapply to calculate the average BMI for male and female separately.
 tapply(data.df$BMI,data.df$gender, mean)
 
-# 2.2: Using our.study list, print only the BMI 
-# Get the name of the first researcher in the study ? 
+
+# 2.2: Using our.study list: 
 
 our.study <- list(data=data.df, researchers=c("Lia", "Zoe", "Luca"))
 d <- our.study$data
@@ -27,9 +27,8 @@ our.study$data[ ,2]
 g <- our.study$researchers
 g[1:1]
 
+
 # 2.3: Reproduce the summary by calling appropriate functions individually (min, median, etc.) 
-# Extract the interquartile range IQR from the summary (compare the results to the IQR function). 
-# How does it compare to the standard deviation of the dimers?
 
 # Upload and read in the file ‘hannes_results_sorted.dat’
 interfaces <- read.table("hannes_results_sorted.dat")
@@ -68,6 +67,38 @@ median(monomers_area)
 quantile(monomers_area,0.25)
 
 quantile(monomers_area,0.75)
+
+# Extract interquartile range from summary and compare result to IQR function
+s <- summary (monomers_area) ## Store the results of summary() in the variable 's'
+
+s[2] ## Extract the first quartile or second element stored in the variable 's'
+
+s[5] ## Extract the third quartile or fifth element stored in the variable s
+
+iqr_monomers <- s[5] - s[2] # Calculate the difference between third and first quartile and store in the variable iqr_monomers
+
+(IQR(monomers_area)) ## IQR function to know the interquartile range
+
+## The result of iqr_monomers is the same than IQR function, because the interquartile range is the difference between the third and first quartile.
+# I do the same for the dimers_area dataset:
+s_d <- summary(dimers_area)
+
+iqr_dimers <- s_d[5] - s_d[2]
+
+(IQR(dimers_area))
+# The result of iqr_dimers is the same than IQR function as expected
+
+
+# Compare to the standard deviation of the dimers
+
+(IQR(dimers_area))
+
+sd(dimers_area)
+
+# Both measures, IQR and sd, indicate amount of variability. IQR consider the variability within 50% of the dataset and standard 
+# deviation consider every single value. In skewed distributions is not possible to calculate standard deviation from IQR,
+# because there are more outliers or extreme values and standard deviation take into account variation in all values.
+# With a normal distribution, not skewed, is possible to calculate the standard deviation from the IQR. In this case, it is skewed.
 
 # The interquartile range is a robust estimator of the standard deviation. 
 # Robust means it is less prone to outliers.
